426590440e4451bdda3ca65584cd6670429cdc38
Ivan Crnogorac <ivan.crnogorac@rubikscode.net>
2025-03-11 18:46:59 +0100

    Card moved to separate component

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,7 @@ import {getTokenListService} from "@/services/services";
 import {Avatar, AvatarFallbackText, AvatarImage} from "@/components/ui/avatar";
 import {formatNumber} from "@/utils/utils";
 import {ITokenListItem} from "@/utils/types";
+import {BullPenTokenCard} from "@/components/ui/bullpenTokenCard";
 
 
 export default function Index() {
@@ -28,7 +29,8 @@ export default function Index() {
 
 
     useEffect(() => {
-        const filteredList = tokenList.filter((token:ITokenListItem)=>(token.address === searchText));
+        const filteredList = tokenList.filter((token:ITokenListItem)=>(token.address.includes(searchText)));
+        console.log('search text: ', searchText);
         console.log('filteredList: ', filteredList);
         if (filteredList.length) {
             setFilteredTokenList(filteredList);
@@ -37,19 +39,30 @@ export default function Index() {
         }
     }, [searchText, tokenList]);
 
-    const renderSearchFiled = () => (
-        <Input variant="outline" size="md">
-            <InputSlot>
-                <InputIcon as={SearchIcon} color="black" size="md" />
-            </InputSlot>
-            <InputField value={searchText} placeholder="Search here..." onChangeText={(newText)=>setSearchText(newText)} />
-            <InputSlot>
-                <Pressable onPress={() => setSearchText("")}>
-                    <InputIcon as={CloseIcon} color="gray" size="md" />
-                </Pressable>
-            </InputSlot>
-        </Input>
-    )
+
+    const renderSearchField = () => {
+        return (
+            <Input
+                size="md"
+                className={`p-2 bg-primaryCardBackground border-primaryBorder mb-2 h-16 rounded-xl`}
+            >
+                <InputSlot>
+                    <InputIcon as={SearchIcon} className={'text-gray-50 '} size="md" />
+                </InputSlot>
+                <InputField
+                    value={searchText}
+                    placeholder="Search Solana address here..."
+                    onChangeText={(newText) => setSearchText(newText)}
+                    className="text-white"
+                />
+                <InputSlot>
+                    <Pressable onPress={() => setSearchText("")}>
+                        <InputIcon as={CloseIcon} className={'text-gray-50'} size="md" />
+                    </Pressable>
+                </InputSlot>
+            </Input>
+        );
+    };
 
 
     const renderEmptyState = () => (
@@ -62,46 +75,14 @@ export default function Index() {
     const renderList = () => (
         <ScrollView className={'overflow-hidden'}>
             {filteredTokeList.length && filteredTokeList.map((token: ITokenListItem) => (
-                <View key={token.address} className={'flex flex-row w-full rounded-xl h-20 bg-primaryCardBackground mt-4 items-center p-4'}>
-                    <View className={'flex w-[15%]'}>
-                        <Avatar size="md">
-                            <AvatarFallbackText>{token.symbol}</AvatarFallbackText>
-                            <AvatarImage
-                                source={{
-                                    uri: token.logoURI,
-                                }}
-                            />
-                        </Avatar>
-                    </View>
-                    <View className={'flex w-[63%]'}>
-                        <View className={'flex flex-col overflow-hidden'}>
-                            <View className={'flex flex-row items-center overflow-hidden'}>
-                                <Text className={'text-xl text-white font-bold'}>{token.symbol}</Text>
-                                <Text className={'pl-2 text-sm text-gray-50 truncate'}>{token.name}</Text>
-                            </View>
-                            <ScrollView horizontal={true} className={'flex flex-row'}>
-                                <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>P:{Number(token.price).toFixed(2)}$</Text>
-                                <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>L:{formatNumber(token.liquidity)}</Text>
-                                <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>V:{formatNumber(token.v24hUSD)}</Text>
-                            </ScrollView>
-                        </View>
-                    </View>
-                    <View className={'flex w-[22%] pl-2 items-end'}>
-                        <Text className={'text-lg text-white font-bold'}>${formatNumber(token.v24hUSD)}</Text>
-
-                        {token?.v24hChangePercent &&
-                            <Text className={`flex flex-row text-xs text-gray-50`}>
-                            24h <Text className={`ml-2 ${token.v24hChangePercent > 0 ? 'text-primaryGreen' : 'text-red-600'}`}>{token?.v24hChangePercent?.toFixed(1)}%</Text>
-                        </Text>}
-                            </View>
-                </View>
+                <BullPenTokenCard  key={token.address} token={token}/>
             ))}
         </ScrollView>
     )
 
   return (
     <View className={'flex flex-1 flex-col items-center justify-center'}>
-        {renderSearchFiled()}
+        {renderSearchField()}
         {filteredTokeList.length ? renderList() : renderEmptyState()}
     </View>
   );
---
+++ components/ui/bullpenTokenCard/index.tsx
@@ -0,0 +1,46 @@
+import {ScrollView, Text, View} from "react-native";
+import {Avatar, AvatarFallbackText, AvatarImage} from "@/components/ui/avatar";
+import {formatNumber} from "@/utils/utils";
+import {ITokenListItem} from "@/utils/types";
+
+interface IProps {
+    token: ITokenListItem
+}
+
+export function BullPenTokenCard ({token}: IProps): JSX.Element {
+    return (
+        <View className={'flex flex-row w-full rounded-xl h-20 bg-primaryCardBackground mt-4 items-center p-4'}>
+            <View className={'flex w-[15%]'}>
+                <Avatar size="md">
+                    <AvatarFallbackText>{token.symbol}</AvatarFallbackText>
+                    <AvatarImage
+                        source={{
+                            uri: token.logoURI,
+                        }}
+                    />
+                </Avatar>
+            </View>
+            <View className={'flex w-[63%]'}>
+                <View className={'flex flex-col overflow-hidden'}>
+                    <View className={'flex flex-row items-center overflow-hidden'}>
+                        <Text className={'text-xl text-white font-bold'}>{token.symbol}</Text>
+                        <Text className={'pl-2 text-sm text-gray-50 truncate'}>{token.name}</Text>
+                    </View>
+                    <ScrollView horizontal={true} className={'flex flex-row'}>
+                        <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>P:{Number(token.price).toFixed(2)}$</Text>
+                        <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>L:{formatNumber(token.liquidity)}</Text>
+                        <Text className={'text-xs text-white border border-secondaryBorder rounded-md p-2 m-1'}>V:{formatNumber(token.v24hUSD)}</Text>
+                    </ScrollView>
+                </View>
+            </View>
+            <View className={'flex w-[22%] pl-2 items-end'}>
+                <Text className={'text-lg text-white font-bold'}>${formatNumber(token.v24hUSD)}</Text>
+
+                {token?.v24hChangePercent &&
+                    <Text className={`flex flex-row text-xs text-gray-400`}>
+                        24h <Text className={`ml-2 ${token.v24hChangePercent > 0 ? 'text-primaryGreen' : 'text-red-600'}`}>{token?.v24hChangePercent?.toFixed(1)}%</Text>
+                    </Text>}
+            </View>
+        </View>
+    )
+}
